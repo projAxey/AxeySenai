d14027712171473238307a331c8d1c505637ee0f
Affonso Davi da Silva <affonso.davi@neomind.com.br>
2024-08-06 19:24:09 -0300

    atualizações

--- padroes/footer.php
+++ padroes/footer.php
@@ -1,8 +1,3 @@
-<style>
-    .bg-footer {
-        background-color: #012640;
-    }
-</style>
 <footer class="bg-footer text-white mt-5">
     <div class="container py-4">
         <div class="row">
@@ -32,9 +27,7 @@
         </div>
     </div>
 </footer>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 
 </html>
