b456157fafb3dc838144b4f0e8d443045da87262
rossinicostavinicius <cansadoelite@gmail.com>
2024-08-07 21:39:29 -0300

    update: calendario

--- paginas/geral/calendario.php
+++ paginas/geral/calendario.php
@@ -3,35 +3,30 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Torna a página responsiva -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendário de Eventos</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.css' />
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        /* Torna o calendário responsivo, ocupando toda a largura disponível em telas menores */
         #calendar {
             width: 100%;
-            max-width: 1000px;
-            /* Limita a largura máxima em telas maiores */
-            margin: 40px auto;
+            max-width: 1200px;
+            /* Aumentado de 1000px para 1200px */
+            margin: 20px auto;
         }
 
-        /* Ajusta o espaçamento abaixo da barra de ferramentas do cabeçalho */
         .fc-header-toolbar {
             margin-bottom: 20px;
         }
 
-        /* Aumenta o espaçamento entre os botões de navegação do calendário */
         .fc-button {
             margin: 0 10px;
         }
 
-        /* Transforma o texto do título do mês para maiúsculas */
         .fc-center h2 {
             text-transform: uppercase;
         }
 
-        /* Estilos personalizados para os botões */
         .btn-custom-blue {
             background-color: #1B3C54;
             color: #FFFFFF;
@@ -48,41 +43,33 @@
             border: 1px solid #1B3C54;
         }
 
-        /* Impede o redimensionamento da textarea */
         textarea {
             resize: none;
         }
 
-        /* Define a largura da modal para 50% da tela e centraliza */
         .modal-dialog {
-            max-width: 50%;
-            /* Ocupa 2/4 ou 50% da tela */
+            max-width: 70%;
+            /* Aumentado de 60% para 70% */
             margin: 30px auto;
-            /* Centraliza a modal */
         }
 
-        /* Tamanho da fonte do título do mês para dispositivos maiores */
         .fc-center h2 {
             font-size: 1.5rem;
         }
 
-        /* Media queries para dispositivos menores */
         @media (max-width: 768px) {
             #calendar {
                 margin: 20px auto;
                 width: 100%;
-                font-size: 0.85rem;
-                /* Reduz o tamanho da fonte */
+                font-size: 0.9rem;
             }
 
             .fc-header-toolbar {
                 font-size: 0.9rem;
-                /* Ajusta o tamanho da fonte da toolbar */
             }
 
             .modal-dialog {
-                max-width: 90%;
-                /* Aumenta a largura da modal para telas menores */
+                max-width: 100%;
                 margin: 20px;
             }
 
@@ -92,111 +79,111 @@
 
             .fc-center h2 {
                 font-size: 1.25rem;
-                /* Reduz o tamanho do título do mês */
             }
         }
 
-        /* Media queries para dispositivos ainda menores */
         @media (max-width: 480px) {
             .fc-center h2 {
                 font-size: 1rem;
-                /* Reduz ainda mais o tamanho do título do mês */
+            }
+        }
+
+        /* Estilo para a modal de adicionar serviço */
+        .modal-dialog-sm {
+            max-width: 30%;
+            margin: 30px auto;
+        }
+
+        @media (max-width: 768px) {
+            .modal-dialog-sm {
+                max-width: 80%;
+            }
+        }
+
+        @media (max-width: 480px) {
+            .modal-dialog-sm {
+                max-width: 95%;
             }
         }
     </style>
 </head>
 
 <body>
-    <div class="container calendar">
-        <div id='calendar'>
-            <!-- Modal para adicionar evento -->
-            <div class="modal fade" id="eventModal" tabindex="-1" role="dialog">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Adicionar Serviço</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <form id="eventForm">
-                                <div class="form-group">
-                                    <label for="description">Descrição do Serviço</label>
-                                    <textarea class="form-control" id="description" name="description" rows="3"
-                                        required></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <label for="start">Data/Hora de Início</label>
-                                    <input type="datetime-local" class="form-control" id="start" name="start" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="end">Data/Hora de Fim</label>
-                                    <input type="datetime-local" class="form-control" id="end" name="end" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="title">Título do Serviço</label>
-                                    <input type="text" class="form-control" id="title" name="title" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="start">Data/Hora de Início</label>
-                                    <div class="input-group">
-                                        <input type="datetime-local" class="form-control" id="start" name="start"
-                                            required>
-                                        <div class="input-group-append">
-                                            <select class="form-control" id="startType">
-                                                <option value="datetime-local">Data e Hora</option>
-                                                <option value="date">Data</option>
-                                                <option value="time">Hora</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="end">Data/Hora de Fim</label>
-                                    <div class="input-group">
-                                        <input type="datetime-local" class="form-control" id="end" name="end" required>
-                                        <div class="input-group-append">
-                                            <select class="form-control" id="endType">
-                                                <option value="datetime-local">Data e Hora</option>
-                                                <option value="date">Data</option>
-                                                <option value="time">Hora</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-custom-white" data-dismiss="modal">Cancelar</button>
-                            <button type="button" class="btn btn-custom-blue" id="saveEvent">Salvar</button>
-                        </div>
+    <div class="container">
+        <button type="button" class="btn btn-primary mt-5" data-toggle="modal" data-target="#calendarModal">
+            Abrir Calendário
+        </button>
+
+        <div class="modal fade" id="calendarModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Agenda de Serviços</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div id='calendar'></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-custom-white" data-dismiss="modal">Fechar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Modal para adicionar evento -->
+        <div class="modal fade" id="eventModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-dialog-sm" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Adicionar Serviço</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="eventForm">
+                            <div class="form-group">
+                                <label for="description">Descrição do Serviço</label>
+                                <textarea class="form-control" id="description" name="description" rows="3" required></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="start">Data/Hora de Início</label>
+                                <input type="datetime-local" class="form-control" id="start" name="start" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="end">Data/Hora de Fim</label>
+                                <input type="datetime-local" class="form-control" id="end" name="end" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="title">Título do Serviço</label>
+                                <input type="text" class="form-control" id="title" name="title" required>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-custom-white" data-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-custom-blue" id="saveEvent">Salvar</button>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'>
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'>
-    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.15/index.global.min.js'></script>
-    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/daygrid@6.1.15/index.global.min.js'></script>
-    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/timegrid@6.1.15/index.global.min.js'></script>
-    <script src='https://cdn.jsdelivr.net/npm/@fullcalendar/core@6.1.15/locales-all.global.min.js'></script>
-
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/fullcalendar.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.0/locale/pt-br.js'></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
     <script>
         $(document).ready(function () {
             $('#calendar').fullCalendar({
                 locale: 'pt-br',
+                height: 'auto',
                 editable: true,
                 header: {
-                    height: '100%',
                     left: 'prev,next today',
                     center: 'title',
                     right: 'month,agendaWeek,agendaDay'
@@ -204,11 +191,19 @@
                 selectable: true,
                 selectHelper: true,
                 select: function (start, end) {
+                    var now = moment().startOf('day');
+                    if (moment(start).isBefore(now)) {
+                        alert('Você não pode selecionar uma data anterior ao dia de hoje.');
+                        $('#calendar').fullCalendar('unselect');
+                        return;
+                    }
                     $('#eventModal').modal('show');
-                    $('#start').val(start.format('YYYY-MM-DDTHH:mm'));
-                    $('#end').val(end.format('YYYY-MM-DDTHH:mm'));
+                    $('#start').val(moment(start).format('YYYY-MM-DDTHH:mm'));
+                    $('#end').val(moment(end).format('YYYY-MM-DDTHH:mm'));
+
                     $('#saveEvent').off('click').on('click', function () {
                         var title = $('#title').val();
+                        var description = $('#description').val();
                         var start = $('#start').val();
                         var end = $('#end').val();
                         if (title) {
@@ -217,6 +212,7 @@
                                 method: 'POST',
                                 data: {
                                     title: title,
+                                    description: description,
                                     start: start,
                                     end: end
                                 },
@@ -231,21 +227,10 @@
                     });
                 }
             });
-
-            $('#startType').change(function () {
-                var selectedType = $(this).val();
-                $('#start').attr('type', selectedType);
-            });
-
-            $('#endType').change(function () {
-                var selectedType = $(this).val();
-                $('#end').attr('type', selectedType);
-            });
         });
     </script>
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // Aqui você colocaria seu código para inserir no banco de dados
         echo "<script>alert('Serviço inserido com sucesso!');</script>";
     }
     ?>
