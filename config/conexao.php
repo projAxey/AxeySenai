d8184e53ad0719abe3948ca6cf6e5fddc933a685
viniciuspavesi <vinicius.paavesi@gmail.com>
2024-09-02 20:49:36 -0300

    Back

--- config/conexao.php
+++ config/conexao.php
@@ -1,16 +1,19 @@
-<?
+<?php
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *'); // habilitar CORS a conexao - util pra função para usar os numeros de tel de maneira dinamica 
+header('Access-Control-Allow-Origin: *');
 
-$servername = "";
-$username = "";
+$servername = "localhost";
+$username = "root";
 $password = "";
-$dbname = "";
+$dbname = "axey_senai";
 
 $conexao = new mysqli($servername, $username, $password, $dbname);
 
 if ($conexao->connect_error) {
     http_response_code(500);
-    echo json_encode(["error" => "Falha na conexão"]);
+    echo json_encode(["error" => "Falha na conexão: " . $conexao->connect_error]);
     exit();
 }
+
+$conexao->close();
+?>
