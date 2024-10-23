0b39e7fd6252f790b4fd2807fe3c4eabab4803e1
Affonso Davi <affonsodavi@gmail.com>
2024-10-23 20:18:17 -0300

    ATT

--- backend/calendario/disponibilidadeInserir.php
+++ backend/calendario/disponibilidadeInserir.php
@@ -1,9 +1,5 @@
 <?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-include_once "/projAxeySenai/config/conexao.php";
+include "/projAxeySenai/config/conexao.php";
 
 header('Content-Type: application/json'); // Definindo o tipo de conteúdo da resposta como JSON
 
