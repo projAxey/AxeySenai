51da7f0db214116ad5a11d7fc800f2cf56f6f3b8
Fernando Warmling <warmlingfernando@gmail.com>
2024-10-29 21:02:20 -0300

    Atualização de paginas de Clientes

--- frontend/agendamentos/agendaModalCliente.php
+++ frontend/agendamentos/agendaModalCliente.php
@@ -5,38 +5,32 @@
 
 <!-- O Formulário Pop-up -->
 <div id="popupForm" class="popup-form popup-form-calendar">
-    <h3>Serviço</h3>
+    <h3>Especificação de Serviços</h3>
     <form id="cadastroDisponibilidade" action="javascript:void(0);"> <!-- Mudei para evitar o envio normal -->
         <!-- <div class="mb-3 visually-hidden"> -->
+            <div class="mb-3 ">
+            <label for="nomeProduto" class="form-label">Serviço</label>
+            <input type="text" id="nomeProduto" name="nomeProduto" class="form-control" readonly>
+        </div>
+        <!-- <div class="mb-3 visually-hidden"> -->
         <div class="mb-3 ">
-            <label for="idAgendamento" class="form-label">ID Fornecedor</label>
-            <input type="number" id="idAgendamento" name="idAgendamento" class="form-control">
-        </div>
-        <div class="mb-3 visually-hidden">
-        <!-- <div class="mb-3 "> -->
-            <label for="idDisponibilidade" class="form-label">id_disponibildiade</label>
-            <input type="number" id="idDisponibilidade" name="idDisponibilidade" class="form-control">
+            <label for="categoriaProduto" class="form-label">Categoria</label>
+            <input type="text" id="categoriaProduto" name="categoriaProduto" class="form-control" readonly>
         </div>
         <div class="mb-3">
-            <label for="startserviceDate" class="form-label">Data Início</label>
-            <input type="date" id="startserviceDate" name="startDayDate" class="form-control">
+            <label for="serviceDate" class="form-label">Data Prevista Prestação</label>
+            <input type="date" id="serviceDate" name="serviceDate" class="form-control" readonly>
         </div>
         <div class="mb-3">
-            <label for="endserviceDate" class="form-label">Data Fim</label>
-            <input type="date" id="endserviceDate" name="endDayDate" class="form-control">
+            <label for="nomePrestador" class="form-label">Prestador Do Serviço</label>
+            <input type="text" id="nomePrestador" name="nomePrestador" class="form-control" readonly>
         </div>
-        <div class="row mb-3">
-            <div class="col">
-                <label for="eventHoraInicio" class="form-label">Hora Início</label>
-                <input type="time" id="eventHoraInicio" name="startTime" class="form-control">
-            </div>
-            <div class="col">
-                <label for="eventHoraFim" class="form-label">Hora Fim</label>
-                <input type="time" id="eventHoraFim" name="endTime" class="form-control">
-            </div>
+        <div class="mb-3 ">
+            <label for="descricaoServico" class="form-label">Descrição do Serviço</label>
+            <!-- <input type="text" id="descricaoServico" name="descricaoServico" class="form-control"> -->
+            <textarea class="form-control" id="descricaoServico" name="descricaoServico" disabled readonly></textarea>
         </div>
         <div class="d-flex justify-content-between">
-            <button type="submit" id="saveEventDisponibilidade" class="btn btn-primary" style="width: 45%;">Salvar</button>
             <button type="button" id="close-cadastro-disponibilidade" class="btn btn-secondary" style="width: 45%;">Fechar</button>
         </div>
     </form>
