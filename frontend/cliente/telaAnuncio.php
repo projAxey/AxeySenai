6ea9d97e642258cd04b91c0eac38254e7b379221
Affonso Davi <affonsodavi@gmail.com>
2024-10-31 21:21:06 -0300

    ok

--- frontend/cliente/telaAnuncio.php
+++ frontend/cliente/telaAnuncio.php
@@ -129,8 +129,7 @@ $servico = $stmtServico->fetch(PDO::FETCH_ASSOC);
 
             </div>
         </div>
-
-        <!-- Modal para login ou criação de conta -->
+        
         <!-- Modal para login ou criação de conta -->
         <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered">
